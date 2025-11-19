760d1dd7704070560957d2e4706cc6ab4948b409
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2025-11-19 18:40:38 +0530

    print hello word, datatypes

---
+++ variaableDemo.java
@@ -0,0 +1,32 @@
+class variaableDemo
+{
+public static void main(String[] args)
+  {
+    byte a=10;
+    byte A=30;
+    short b=20;
+    int num=3;
+    char ch='a';
+    float ht=5.3f;
+    double d=3.333; 
+    String name ="Mayuri";
+    long mobNo = 96993198;
+    boolean status= true;
+ 
+    //System.out.println("Hello Mayuraa!!!!");
+      System.out.println("value of byte : "+a);
+      System.out.println("value of short : "+b);
+      System.out.println("value of int : "+num);
+      System.out.println("value of char : "+ch);
+      System.out.println("value of float : "+ht);
+      System.out.println("value of double : "+d);
+      System.out.println("value of string : "+name);
+      System.out.println("value of long : "+mobNo);
+      System.out.println("value of boolean : "+status);
+ 
+       
+
+  }
+
+
+}
